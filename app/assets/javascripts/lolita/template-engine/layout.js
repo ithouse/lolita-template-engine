3810a447bbb068afd67f22bb6405a859cfc7d303
Arturs Meisters <arturs@ithouse.lv>
2011-12-01 14:22:42 +0200

    bug fix in js

--- app/assets/javascripts/lolita/template-engine/layout.js
+++ app/assets/javascripts/lolita/template-engine/layout.js
@@ -138,113 +138,261 @@ $(window).resize(function(){
   resize_all_elements();
 })
 
+
+var _activated_cb = [];
+var _enabled_ph_class = ".placeholder.enabled"
+var _all_ph_class = ".placeholder"
+var _disabled_ph_class = ".placeholder.disabled"
+var _form_class = "#placeholders-form"
+var _default_font_size = 13;
+
+function is_existing_layout(){
+  $(".tabs").data("method") == "PUT"
+}
+
+//Detect if block is meant for placeholder, e.g., some blocks may be meant for specifict placeholders
+function block_meant_for_placeholder($p_holder,$block){
+  return ($block.hasClass("fit-in-all") || $block.hasClass($p_holder.data("name")))
+}
+
+//Check if content block fits in placeholder.
+//When placeholder are allowed to stretch vertically or horizontally then checking of height or width are skipped.
+function fit_in_placeholder(placeholder,content_block){
+  if(block_meant_for_placeholder(placeholder,content_block)){
+    var p_width = parseInt(placeholder.attr("data-width")) ;
+    var p_height = placeholder.height();
+    var p_stretch = placeholder.data("stretch")
+    var result = true
+    if(p_stretch!="vertically"){
+      result = result && p_height <=parseInt(placeholder.data("height"))
+    }
+    if(p_stretch!="horizontally"){
+      result = result && content_block.attr("data-width") <= p_width && placeholder.width() <= parseInt(placeholder.data("width"))
+    }else{
+      if(content_block.width() > placeholder.width()){
+        placeholder.width(content_block.width())
+      }
+    }
+    return result 
+  }else{
+    return false
+  }
+}
+
+//Reset content block look
+function reset_block($block){
+  $block.removeClass("active");
+  $block.addClass("inactive");
+  $block.children("span:first").css({"font-size":_default_font_size,opacity:1})
+  $block.height($block.data("old-height"));
+  $block.width($block.data("old-width"))
+  return $block
+}
+
+// Add clone back in content blocks list and revert look to original.
+function add_clone($clone){
+  if($clone.data("domPositionPrev")){
+    $clone.data("domPositionObj").after($clone)
+  }else{
+    $clone.data("domPositionObj").prepend($clone)
+  }
+  $clone.data("domPositionPrev",false)
+  $clone.data("domPositionObj",false)
+  return reset_block($clone)
+}
+
+//Make sortable placeholders look like content block
+function fix_real_placeholder(ui){
+  $block = ui.item
+  $real_ph = $(".inner-placeholder")
+  $real_ph.width($block.width());
+  $real_ph.height($block.height());  
+  $real_ph.css("cssFloat","left")
+}
+
+// Return array of clones of _destroy inputs and id inputs, thoes can be used to add back to form
+function collect_removed_elements($blocks,skip_old){
+  var clones = !skip_old ? collect_removed_elements($(_form_class + " #removed-cbs-container>div"),true) : []
+  $blocks = $blocks || $(_form_class + " .content-block")
+  $blocks.each(function(){
+    var $destroy = $(this).children("input[id*='__destroy']:last")
+    var $id = $(this).next()
+    if($destroy.length>0 && $id[0] && $id[0].tagName.toLowerCase()=="input" && $id.attr("id").match(/_id$/)){
+      clones.push([$destroy.clone(),$id.clone()])
+    }
+  })
+  return clones
+}
+
+// Receive array of arrays, where each inner array container two jquery objects.
+// First is clone of _destroy input for removed content block and second is clone of id input of removed 
+// content block. It creates container with id 'removed-cbs-container' ad each _destroy input is put in
+// another div, so this DOM structure matches real form structure, for less complex logic for cloning in
+// #collect_removed_elements()
+function remove_blocks(block_clones){
+  var $removed_container = $(_form_class+" #removed-cbs-container")
+  if(!$removed_container[0]){
+    $(_form_class).append("<div id='removed-cbs-container'></div>")
+    $removed_container = $(_form_class).find("#removed-cbs-container")
+  }
+  $.each(block_clones,function(index,elements){
+    elements[0].val(1)
+    $removed_container.append("<div></div>")
+    var $delete_container = $removed_container.find(":last")
+    $delete_container.append(elements[0])
+    $removed_container.append(elements[1])
+  })
+}
+
+function rebuild_content_block_form_data($block,attributes){
+  if(attributes){
+    for(var attr in attributes){
+      var input = $block.children("input[id*='"+attr+"']").eq(0)
+      if(input){
+        input.val(attributes[attr])
+      }
+    } 
+  }
+  if($block.data("content-block-id")){
+    $block.children("input[id*='lolita_content_block_id']").eq(0).val($block.data("content-block-id"))
+  }else{
+    $block.children("input[id*='predefined_block_name']").eq(0).val($block.data("name")) 
+  }
+}
+
+function get_form_data($block,attr){
+  return $block.children("input[id*='"+attr+"']").eq(0).val()
+}
+
+function initialize_sortables(){
+  //Placeholders also are sortable but they only changes sortable placeholder look on start and check fitnes of 
+  //block in placeholder.
+  $(_enabled_ph_class).sortable({
+    connectWith: _enabled_ph_class,
+    placeholder: "inner-placeholder",
+    cursor: "move",
+    items: ".content-block",
+    start:function(event,ui){
+      
+      fix_real_placeholder(ui)
+    },
+    stop: function(event,ui){
+      
+      var $block = ui.item;
+      var $p_holder = ui.item.parent();
+      if(!fit_in_placeholder($p_holder,$block)){
+        $(this).sortable("cancel");
+      }else{
+        rebuild_order_numbers_for($p_holder)
+      }
+    }
+  }).disableSelection();
+
+  $("#content-blocks").sortable({
+    connectWith: _enabled_ph_class,
+    placeholder: 'inner-placeholder',
+    cursor: "move",
+    items: ".content-block",
+    //Block started state are changed to true and sortable placeholder look are changed to match block look.
+    start:function(event,ui){
+      ui.item.data("started",true)
+      fix_real_placeholder(ui)
+    },
+    // When sorting stops system checks if block fits in container and if its true it is placed inside it
+    // and clone of it are placed back in list with previous look. When it not fits in then sortable cancel
+    // action is called and block look are restored.
+    stop:function(event,ui){
+      ui.item.data("started",false)
+      var $block = ui.item;
+      var $p_holder = ui.item.parent();
+      if($p_holder.hasClass("placeholder") && fit_in_placeholder($p_holder,$block)){
+        var $clone = $block.clone(true);
+        $block.unbind("mousedown");
+        add_form_data($p_holder,$block);
+        add_clone($clone) 
+        center_spans($block.children("span:first"))
+      }else{
+        $(this).sortable("cancel");
+        reset_block($block)
+      }
+    }
+  }).disableSelection();
+}
+
+function rebuild_order_numbers_for($placeholder){
+    $placeholder.find("input[id*='order_number']").each(function(index){
+      $(this).val(index)
+    })  
+  }
+
+// Set theme namd and layout name in form
+function set_layout_form_data(theme,layout,title,urls){
+  $("#lolita_layout_theme_name").val(theme)
+  $("#lolita_layout_name").val(layout)
+  if(title){
+    $("#lolita_layout_title").val(title)
+  }  
+  if(urls){
+    $(_form_class + " .nested_form:first").replaceWith(urls)
+  }
+}
+
+// Copy form data form pre-defined placeholder nested form 
+// Add values to hidden fields about where CB is placed and what is its name
+function add_form_data($placeholder,$block){
+  var form_data = $placeholder.data("form");
+  var new_id = new Date().getTime();
+  var regexp = new RegExp("new_" + "layout_configurations", "g")
+  form_data = form_data.replace(regexp,new_id)
+  $block.append(form_data)
+  rebuild_content_block_form_data($block)
+  rebuild_order_numbers_for($placeholder)
+}
+
+function initialize_dialog(){
+  var buttons = {}
+  buttons[$("#content-block-form").data("button-save")] = function(){
+    var $block = $(this).data("block")
+    var method_name = $(this).find("select>option:selected").eq(0).val()
+    rebuild_content_block_form_data($block,{"data_collection_method": method_name})
+    $( this ).dialog( "close" );
+  }
+  buttons[$("#content-block-form").data("button-cancel")] = function(){
+    $( this ).dialog( "close" );
+  }
+  $("#content-block-form" ).dialog({
+      autoOpen: false,
+      height: 150,
+      width: 300,
+      modal: true,
+      buttons: buttons,
+      open:function(){
+        var $select = $(this).find("form select")
+        $select.html("")
+        $.each($(this).data("methods"),function(index,method_name){
+          $select.append("<option value='"+method_name+"'>"+method_name+"</option>")
+        })
+        var $block = $(this).data("block")
+        $select.val(get_form_data($block,"data_collection_method"))
+      },
+      close: function() {
+        $(this).find("form select").html("")
+        //allFields.val( "" ).removeClass( "ui-state-error" );
+      }
+  });
+}
+
+function initialize_layout_config(){
+  resize_all_elements();
+  initialize_sortables();
+  initialize_dialog();
+}
+  
+  //Initialize placeholder dimensions
+  //initialize_placeholders();
+
 $(function(){
   windowScrollDimensions()
-  var _activated_cb = [];
-  var _enabled_ph_class = ".placeholder.enabled"
-  var _all_ph_class = ".placeholder"
-  var _disabled_ph_class = ".placeholder.disabled"
-  var _form_class = "#placeholders-form"
-  var _default_font_size = 13;
-  
-  function is_existing_layout(){
-    $(".tabs").data("method") == "PUT"
-  }
-
-  //Detect if block is meant for placeholder, e.g., some blocks may be meant for specifict placeholders
-  function block_meant_for_placeholder($p_holder,$block){
-    return ($block.hasClass("fit-in-all") || $block.hasClass($p_holder.data("name")))
-  }
-
-  //Check if content block fits in placeholder.
-  //When placeholder are allowed to stretch vertically or horizontally then checking of height or width are skipped.
-  function fit_in_placeholder(placeholder,content_block){
-    if(block_meant_for_placeholder(placeholder,content_block)){
-      var p_width = parseInt(placeholder.attr("data-width")) ;
-      var p_height = placeholder.height();
-      var p_stretch = placeholder.data("stretch")
-      var result = true
-      if(p_stretch!="vertically"){
-        result = result && p_height <=parseInt(placeholder.data("height"))
-      }
-      if(p_stretch!="horizontally"){
-        result = result && content_block.attr("data-width") <= p_width && placeholder.width() <= parseInt(placeholder.data("width"))
-      }else{
-        if(content_block.width() > placeholder.width()){
-          placeholder.width(content_block.width())
-        }
-      }
-      return result 
-    }else{
-      return false
-    }
-  }
- 
-  //Reset content block look
-  function reset_block($block){
-    $block.removeClass("active");
-    $block.addClass("inactive");
-    $block.children("span:first").css({"font-size":_default_font_size,opacity:1})
-    $block.height($block.data("old-height"));
-    $block.width($block.data("old-width"))
-    return $block
-  }
-
-  // Add clone back in content blocks list and revert look to original.
-  function add_clone($clone){
-    if($clone.data("domPositionPrev")){
-      $clone.data("domPositionObj").after($clone)
-    }else{
-      $clone.data("domPositionObj").prepend($clone)
-    }
-    $clone.data("domPositionPrev",false)
-    $clone.data("domPositionObj",false)
-    return reset_block($clone)
-  }
-
-  //Make sortable placeholders look like content block
-  function fix_real_placeholder(ui){
-    $block = ui.item
-    $real_ph = $(".inner-placeholder")
-    $real_ph.width($block.width());
-    $real_ph.height($block.height());  
-    $real_ph.css("cssFloat","left")
-  }
-
-  // Return array of clones of _destroy inputs and id inputs, thoes can be used to add back to form
-  function collect_removed_elements($blocks,skip_old){
-    var clones = !skip_old ? collect_removed_elements($(_form_class + " #removed-cbs-container>div"),true) : []
-    $blocks = $blocks || $(_form_class + " .content-block")
-    $blocks.each(function(){
-      var $destroy = $(this).children("input[id*='__destroy']:last")
-      var $id = $(this).next()
-      if($destroy.length>0 && $id[0] && $id[0].tagName.toLowerCase()=="input" && $id.attr("id").match(/_id$/)){
-        clones.push([$destroy.clone(),$id.clone()])
-      }
-    })
-    return clones
-  }
-
-  // Receive array of arrays, where each inner array container two jquery objects.
-  // First is clone of _destroy input for removed content block and second is clone of id input of removed 
-  // content block. It creates container with id 'removed-cbs-container' ad each _destroy input is put in
-  // another div, so this DOM structure matches real form structure, for less complex logic for cloning in
-  // #collect_removed_elements()
-  function remove_blocks(block_clones){
-    var $removed_container = $(_form_class+" #removed-cbs-container")
-    if(!$removed_container[0]){
-      $(_form_class).append("<div id='removed-cbs-container'></div>")
-      $removed_container = $(_form_class).find("#removed-cbs-container")
-    }
-    $.each(block_clones,function(index,elements){
-      elements[0].val(1)
-      $removed_container.append("<div></div>")
-      var $delete_container = $removed_container.find(":last")
-      $delete_container.append(elements[0])
-      $removed_container.append(elements[1])
-    })
-  }
-
   $(".nested-form-fields-container .controller_select").live("change",function(){
     var controller = $(this).children("option:selected").eq(0).val();
     var actions = ControllersActions[controller]
@@ -320,114 +468,6 @@ $(function(){
     })
   })
 
-  function rebuild_order_numbers_for($placeholder){
-    $placeholder.find("input[id*='order_number']").each(function(index){
-      $(this).val(index)
-    })  
-  }
-
-  // Set theme namd and layout name in form
-  function set_layout_form_data(theme,layout,title,urls){
-    $("#lolita_layout_theme_name").val(theme)
-    $("#lolita_layout_name").val(layout)
-    if(title){
-      $("#lolita_layout_title").val(title)
-    }  
-    if(urls){
-      $(_form_class + " .nested_form:first").replaceWith(urls)
-    }
-  }
-
-  // Copy form data form pre-defined placeholder nested form 
-  // Add values to hidden fields about where CB is placed and what is its name
-  function add_form_data($placeholder,$block){
-    var form_data = $placeholder.data("form");
-    var new_id = new Date().getTime();
-    var regexp = new RegExp("new_" + "layout_configurations", "g")
-    form_data = form_data.replace(regexp,new_id)
-    $block.append(form_data)
-    rebuild_content_block_form_data($block)
-    rebuild_order_numbers_for($placeholder)
-  }
-
-  function rebuild_content_block_form_data($block,attributes){
-    if(attributes){
-      for(var attr in attributes){
-        var input = $block.children("input[id*='"+attr+"']").eq(0)
-        if(input){
-          input.val(attributes[attr])
-        }
-      } 
-    }
-    if($block.data("content-block-id")){
-      $block.children("input[id*='lolita_content_block_id']").eq(0).val($block.data("content-block-id"))
-    }else{
-      $block.children("input[id*='predefined_block_name']").eq(0).val($block.data("name")) 
-    }
-  }
-
-  function get_form_data($block,attr){
-    return $block.children("input[id*='"+attr+"']").eq(0).val()
-  }
-
-  function initialize_sortables(){
-    //Placeholders also are sortable but they only changes sortable placeholder look on start and check fitnes of 
-    //block in placeholder.
-    $(_enabled_ph_class).sortable({
-      connectWith: _enabled_ph_class,
-      placeholder: "inner-placeholder",
-      cursor: "move",
-      items: ".content-block",
-      start:function(event,ui){
-        
-        fix_real_placeholder(ui)
-      },
-      stop: function(event,ui){
-        
-        var $block = ui.item;
-        var $p_holder = ui.item.parent();
-        if(!fit_in_placeholder($p_holder,$block)){
-          $(this).sortable("cancel");
-        }else{
-          rebuild_order_numbers_for($p_holder)
-        }
-      }
-    }).disableSelection();
-
-    $("#content-blocks").sortable({
-      connectWith: _enabled_ph_class,
-      placeholder: 'inner-placeholder',
-      cursor: "move",
-      items: ".content-block",
-      //Block started state are changed to true and sortable placeholder look are changed to match block look.
-      start:function(event,ui){
-        ui.item.data("started",true)
-        fix_real_placeholder(ui)
-      },
-      // When sorting stops system checks if block fits in container and if its true it is placed inside it
-      // and clone of it are placed back in list with previous look. When it not fits in then sortable cancel
-      // action is called and block look are restored.
-      stop:function(event,ui){
-        ui.item.data("started",false)
-        var $block = ui.item;
-        var $p_holder = ui.item.parent();
-        if($p_holder.hasClass("placeholder") && fit_in_placeholder($p_holder,$block)){
-          var $clone = $block.clone(true);
-          $block.unbind("mousedown");
-          add_form_data($p_holder,$block);
-          add_clone($clone) 
-          center_spans($block.children("span:first"))
-        }else{
-          $(this).sortable("cancel");
-          reset_block($block)
-        }
-      }
-    }).disableSelection();
-  }
-  //Initialize placeholder dimensions
-  //initialize_placeholders();
-  initialize_sortables();
-
    //Remove block on double click from placeholders
   $(".content-block.active span.delete").live("click",function(){
     var $block = $(this).parent()
@@ -460,38 +500,6 @@ $(function(){
     $dialog.data("methods",methods)
     $dialog.dialog("open")
   })
-
-
-  var buttons = {}
-  buttons[$("#content-block-form").data("button-save")] = function(){
-    var $block = $(this).data("block")
-    var method_name = $(this).find("select>option:selected").eq(0).val()
-    rebuild_content_block_form_data($block,{"data_collection_method": method_name})
-    $( this ).dialog( "close" );
-  }
-  buttons[$("#content-block-form").data("button-cancel")] = function(){
-    $( this ).dialog( "close" );
-  }
-  $("#content-block-form" ).dialog({
-      autoOpen: false,
-      height: 150,
-      width: 300,
-      modal: true,
-      buttons: buttons,
-      open:function(){
-        var $select = $(this).find("form select")
-        $select.html("")
-        $.each($(this).data("methods"),function(index,method_name){
-          $select.append("<option value='"+method_name+"'>"+method_name+"</option>")
-        })
-        var $block = $(this).data("block")
-        $select.val(get_form_data($block,"data_collection_method"))
-      },
-      close: function() {
-        $(this).find("form select").html("")
-        //allFields.val( "" ).removeClass( "ui-state-error" );
-      }
-    });
 
   //When user press mouse button on contenblock in list it is changed to its real look. 
   // Also some information about original position in list and dimensions are stored.
